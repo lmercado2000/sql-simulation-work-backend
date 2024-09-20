685c75f4b2a6ed807f1712b114ef0a302d12d5a3
lucasmercado101 <lucasmercado101@gmail.com>
2024-09-20 18:12:22 -0300

    set up express adapter

--- main.ts
+++ main.ts
@@ -1,16 +1,43 @@
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
-import { router } from "./src/trpc";
+import { publicProcedure, router } from "./src/trpc";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import Express from "express";
+import trpc from "@trpc/server";
+import cors from "cors";
+
+const App = Express();
 
 const appRouter = router({
-  // ...
+  userList: publicProcedure.query(async () => {
+    console.log("CALLEd");
+    // Retrieve users from a datasource, this is an imaginary database
+    return [];
+  })
 });
 
 export type AppRouter = typeof appRouter;
+// created for each request
+const createContext = ({
+  req,
+  res
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = trpc.inferAsyncReturnType<typeof createContext>;
 
 const server = createHTTPServer({
   router: appRouter
 });
-
-server.listen(3000, () => {
-  console.log("Listening on http://localhost:3000");
+App.use(
+  cors({
+    allowedHeaders: ["*"]
+  })
+);
+App.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: createContext
+  })
+);
+App.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
