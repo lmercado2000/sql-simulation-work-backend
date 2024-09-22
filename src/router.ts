44fba3e1ff607b85701de3d7c4d25793de564319
lucasmercado101 <lucasmercado101@gmail.com>
2024-09-22 15:51:13 -0300

    added zod and fetch allStudents endpoint

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,27 @@
 import { publicProcedure, router } from "../src/trpc";
 import { db } from "./db";
+import { z } from "zod";
+
+const studentSchema = z.object({
+  id: z.number(),
+  status_id: z.number(),
+  first_name: z.string(),
+  last_name: z.string(),
+  sex: z.boolean(),
+  created_at: z.string(),
+  updated_at: z.string()
+});
+
+export type Student = z.infer<typeof studentSchema>;
 
 export const appRouter = router({
-  allStudents: publicProcedure.query(async () => {
-    return (await db.query("SELECT * FROM Students")).rows;
-  })
+  allStudents: publicProcedure
+    .output(z.array(studentSchema))
+    .query(async () => {
+      const students = (await db.query("SELECT * FROM Students"))
+        .rows as Student[];
+      return students;
+    })
 });
 
 export type AppRouter = typeof appRouter;
